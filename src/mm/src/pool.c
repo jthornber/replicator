b02f0d9a791829d61d936d96abc6d67ef398a6eb
Joe Thornber <joe.thornber@gmail.com>
2010-07-05 10:10:03 +0100

    rename pool code, remove the old debug version

--- src/mm/src/pool.c
+++ src/mm/src/pool.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2001-2004 Sistina Software, Inc. All rights reserved.  
+ * Copyright (C) 2001-2004 Sistina Software, Inc. All rights reserved.
  * Copyright (C) 2004-2005 Red Hat, Inc. All rights reserved.
  *
  * This file is part of the device-mapper userspace tools.
@@ -13,21 +13,233 @@
  * Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
-#include "dmlib.h"
+struct chunk {
+	char *begin, *end;
+	struct chunk *prev;
+};
 
-/* FIXME: thread unsafe */
-static DM_LIST_INIT(_dm_pools);
-void dm_pools_check_leaks(void);
+struct pool {
+	struct list list;
+	struct chunk *chunk, *spare_chunk;	/* spare_chunk is a one entry free
+						   list to stop 'bobbling' */
+	size_t chunk_size;
+	size_t object_len;
+	unsigned object_alignment;
+};
 
-#ifdef DEBUG_POOL
-#include "pool-debug.c"
-#else
-#include "pool-fast.c"
-#endif
+void _align_chunk(struct chunk *c, unsigned alignment);
+struct chunk *_new_chunk(struct pool *p, size_t s);
 
-char *dm_pool_strdup(struct dm_pool *p, const char *str)
+/* by default things come out aligned for doubles */
+#define DEFAULT_ALIGNMENT __alignof__ (double)
+
+struct pool *pool_create(const char *name, size_t chunk_hint)
 {
-	char *ret = dm_pool_alloc(p, strlen(str) + 1);
+	size_t new_size = 1024;
+	struct pool *p = malloc(sizeof(*p));
+
+	if (!p) {
+		log_error("Couldn't create memory pool %s (size %"
+			  PRIsize_t ")", name, sizeof(*p));
+		return 0;
+	}
+	memset(p, 0, sizeof(*p));
+
+	/* round chunk_hint up to the next power of 2 */
+	p->chunk_size = chunk_hint + sizeof(struct chunk);
+	while (new_size < p->chunk_size)
+		new_size <<= 1;
+	p->chunk_size = new_size;
+	list_add(&_pools, &p->list);
+	return p;
+}
+
+void pool_destroy(struct pool *p)
+{
+	struct chunk *c, *pr;
+	free(p->spare_chunk);
+	c = p->chunk;
+	while (c) {
+		pr = c->prev;
+		free(c);
+		c = pr;
+	}
+
+	list_del(&p->list);
+	free(p);
+}
+
+void *pool_alloc(struct pool *p, size_t s)
+{
+	return pool_alloc_aligned(p, s, DEFAULT_ALIGNMENT);
+}
+
+void *pool_alloc_aligned(struct pool *p, size_t s, unsigned alignment)
+{
+	struct chunk *c = p->chunk;
+	void *r;
+
+	/* realign begin */
+	if (c)
+		_align_chunk(c, alignment);
+
+	/* have we got room ? */
+	if (!c || (c->begin > c->end) || (c->end - c->begin < s)) {
+		/* allocate new chunk */
+		size_t needed = s + alignment + sizeof(struct chunk);
+		c = _new_chunk(p, (needed > p->chunk_size) ?
+			       needed : p->chunk_size);
+
+		if (!c)
+			return NULL;
+
+		_align_chunk(c, alignment);
+	}
+
+	r = c->begin;
+	c->begin += s;
+	return r;
+}
+
+void pool_empty(struct pool *p)
+{
+	struct chunk *c;
+
+	for (c = p->chunk; c && c->prev; c = c->prev)
+		;
+
+	if (c)
+		pool_free(p, (char *) (c + 1));
+}
+
+void pool_free(struct pool *p, void *ptr)
+{
+	struct chunk *c = p->chunk;
+
+	while (c) {
+		if (((char *) c < (char *) ptr) &&
+		    ((char *) c->end > (char *) ptr)) {
+			c->begin = ptr;
+			break;
+		}
+
+		if (p->spare_chunk)
+			free(p->spare_chunk);
+		p->spare_chunk = c;
+		c = c->prev;
+	}
+
+	if (!c)
+		log_error(INTERNAL_ERROR "pool_free asked to free pointer "
+			  "not in pool");
+	else
+		p->chunk = c;
+}
+
+int pool_begin_object(struct pool *p, size_t hint)
+{
+	struct chunk *c = p->chunk;
+	const size_t align = DEFAULT_ALIGNMENT;
+
+	p->object_len = 0;
+	p->object_alignment = align;
+
+	if (c)
+		_align_chunk(c, align);
+
+	if (!c || (c->begin > c->end) || (c->end - c->begin < hint)) {
+		/* allocate a new chunk */
+		c = _new_chunk(p,
+			       hint > (p->chunk_size - sizeof(struct chunk)) ?
+			       hint + sizeof(struct chunk) + align :
+			       p->chunk_size);
+
+		if (!c)
+			return 0;
+
+		_align_chunk(c, align);
+	}
+
+	return 1;
+}
+
+int pool_grow_object(struct pool *p, const void *extra, size_t delta)
+{
+	struct chunk *c = p->chunk, *nc;
+
+	if (!delta)
+		delta = strlen(extra);
+
+	if (c->end - (c->begin + p->object_len) < delta) {
+		/* move into a new chunk */
+		if (p->object_len + delta > (p->chunk_size / 2))
+			nc = _new_chunk(p, (p->object_len + delta) * 2);
+		else
+			nc = _new_chunk(p, p->chunk_size);
+
+		if (!nc)
+			return 0;
+
+		_align_chunk(p->chunk, p->object_alignment);
+		memcpy(p->chunk->begin, c->begin, p->object_len);
+		c = p->chunk;
+	}
+
+	memcpy(c->begin + p->object_len, extra, delta);
+	p->object_len += delta;
+	return 1;
+}
+
+void *pool_end_object(struct pool *p)
+{
+	struct chunk *c = p->chunk;
+	void *r = c->begin;
+	c->begin += p->object_len;
+	p->object_len = 0u;
+	p->object_alignment = DEFAULT_ALIGNMENT;
+	return r;
+}
+
+void pool_abandon_object(struct pool *p)
+{
+	p->object_len = 0;
+	p->object_alignment = DEFAULT_ALIGNMENT;
+}
+
+void _align_chunk(struct chunk *c, unsigned alignment)
+{
+	c->begin += alignment - ((unsigned long) c->begin & (alignment - 1));
+}
+
+struct chunk *_new_chunk(struct pool *p, size_t s)
+{
+	struct chunk *c;
+
+	if (p->spare_chunk &&
+	    ((p->spare_chunk->end - (char *) p->spare_chunk) >= s)) {
+		/* reuse old chunk */
+		c = p->spare_chunk;
+		p->spare_chunk = 0;
+	} else {
+		if (!(c = malloc(s))) {
+			log_error("Out of memory.  Requested %" PRIsize_t
+				  " bytes.", s);
+			return NULL;
+		}
+
+		c->end = (char *) c + s;
+	}
+
+	c->prev = p->chunk;
+	c->begin = (char *) (c + 1);
+	p->chunk = c;
+
+	return c;
+}
+
+char *pool_strdup(struct pool *p, const char *str)
+{
+	char *ret = pool_alloc(p, strlen(str) + 1);
 
 	if (ret)
 		strcpy(ret, str);
@@ -35,9 +247,9 @@ char *dm_pool_strdup(struct dm_pool *p, const char *str)
 	return ret;
 }
 
-char *dm_pool_strndup(struct dm_pool *p, const char *str, size_t n)
+char *pool_strndup(struct pool *p, const char *str, size_t n)
 {
-	char *ret = dm_pool_alloc(p, n + 1);
+	char *ret = pool_alloc(p, n + 1);
 
 	if (ret) {
 		strncpy(ret, str, n);
@@ -47,31 +259,12 @@ char *dm_pool_strndup(struct dm_pool *p, const char *str, size_t n)
 	return ret;
 }
 
-void *dm_pool_zalloc(struct dm_pool *p, size_t s)
+void *pool_zalloc(struct pool *p, size_t s)
 {
-	void *ptr = dm_pool_alloc(p, s);
+	void *ptr = pool_alloc(p, s);
 
 	if (ptr)
 		memset(ptr, 0, s);
 
 	return ptr;
-}
-
-void dm_pools_check_leaks(void)
-{
-	struct dm_pool *p;
-
-	if (dm_list_empty(&_dm_pools))
-		return;
-
-	log_error("You have a memory leak (not released memory pool):");
-	dm_list_iterate_items(p, &_dm_pools) {
-#ifdef DEBUG_POOL
-		log_error(" [%p] %s (%u bytes)",
-			  p->orig_pool,
-			  p->name, p->stats.bytes);
-#else
-		log_error(" [%p]", p);
-#endif
-	}
 }
