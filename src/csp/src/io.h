92d52d3ead994ebab5f69b899f2d0a671a8ff190
Joe Thornber <joe.thornber@gmail.com>
2010-07-06 10:01:44 +0100

    [CSP] declare {read,write}_exact

--- src/csp/src/io.h
+++ src/csp/src/io.h
@@ -9,6 +9,9 @@ void set_non_blocking(int fd);
 ssize_t csp_read(int fd, void *buf, size_t count);
 ssize_t csp_write(int fd, const void *buf, size_t count);
 
+ssize_t read_exact(int fd, void *buf, size_t count);
+ssize_t write_exact(int fd, const void *buf, size_t count);
+
 /*----------------------------------------------------------------*/
 
 #endif
