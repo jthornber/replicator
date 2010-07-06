63388c32fb2272061913c05eb95ed9d0bb50a074
Joe Thornber <joe.thornber@gmail.com>
2010-07-06 10:03:15 +0100

    [REPLICATOR] write function to read xdr request from socket

--- src/replicator/src/main.c
+++ src/replicator/src/main.c
@@ -1,9 +1,46 @@
 #include "protocol.h"
+#include "csp/control.h"
+#include "csp/io.h"
 
 #include <stdio.h>
 
+/*
+ * Server
+ */
+int read_request(int fd, struct pool *mem, command **result, uint32_t *req_id)
+{
+        request_header header_raw, *header;
+        char data[32 * 1024];   /* FIXME: use a dynamic buffer */
+
+        /* read the header */
+        if (read_exact(fd, &header_raw, sizeof(header)) < 0)
+                return 0;
+
+        if (!xdr_unpack_using(request_header, &header_raw, sizeof(header_raw), mem, &header))
+                return 0;
+
+        /* read the payload */
+        if (read_exact(fd, data, header->msg_size) < 0)
+                return 0;
+
+        if (!xdr_unpack_using(command, data, header->msg_size, mem, result))
+                return 0;
+
+        *req_id = header->request_id;
+        return 1;
+}
+
+/*
+ * Top level
+ */
 int main(int argc, char **argv)
 {
+        /* start the server */
+
+        /* set up the top level csp process */
+
+        /* start the csp scheduler */
+
         printf("hello, world!\n");
         return 0;
 }
