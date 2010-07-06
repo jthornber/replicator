3c89934222e2f03a2c7c4030e9907e1c1122c8de
Joe Thornber <joe.thornber@gmail.com>
2010-07-06 11:03:07 +0100

    [CSP] add chan_pop_one_of

--- src/csp/src/channel.h
+++ src/csp/src/channel.h
@@ -8,8 +8,11 @@ struct channel;
 struct channel *chan_create();
 void chan_inc(struct channel *chan);
 void chan_dec(struct channel *chan);
-void chan_push(struct channel *c, void *data, size_t len);
-void chan_pop(struct channel *c, void **data, size_t *len);
+
+int chan_push(struct channel *c, void *data, size_t len);
+int chan_pop(struct channel *c, void **data, size_t *len);
+int chan_pop_one_of(struct channel **c, unsigned count,
+                    unsigned *index, void **data, size_t *len);
 
 /* FIXME: add support for poison */
 
