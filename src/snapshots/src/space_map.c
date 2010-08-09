e59524bbc041a3ea0f1e2e1f6ea7ccfd46eb36c8
Joe Thornber <joe.thornber@gmail.com>
2010-08-09 16:17:33 +0100

    [SNAPSHOTS] first attempt at making the space map persistent, still issues with locking

--- src/snapshots/src/space_map.c
+++ src/snapshots/src/space_map.c
@@ -1,4 +1,5 @@
-#include "snapshots/space_map.h"
+#include "space_map.h"
+#include "btree.h"
 
 #include "datastruct/list.h"
 
@@ -7,84 +8,259 @@
 #include <stdlib.h>
 #include <string.h>
 
-/* FIXME: throw away implementation for now.  Look at Mikulas' radix trees */
+/* FIXME: Look at Mikulas' radix trees, is this applicable to reference counts? */
+
+/* FIXME: at the very least store ranges in the btree.  Value can have
+ * 32bit reference count + 32 bit range.  Nope range implied by the keys.
+ * Have to be 64bit for reference count :(
+ */
 
 /*----------------------------------------------------------------*/
 
-struct space_map {
-	block_t nr_blocks;
-	block_t last_allocated;
-	uint32_t *ref_count;
+/* we have a little hash table of the reference count changes */
+struct delta_entry {
+	struct list lru;
+	struct list hash;
+	block_t block;
+	int32_t delta;
 };
 
-struct space_map *space_map_create(block_t nr_blocks)
+#define NR_BUCKETS 1024
+#define MASK (NR_BUCKETS - 1)
+#define PRIME 4294967291
+
+struct space_map {
+	struct transaction_manager *tm;
+	block_t nr_blocks;
+	int initialised;
+	block_t root;
+	block_t last_allocated;
+
+	struct list deltas;
+	struct list buckets[NR_BUCKETS];
+};
+
+/* FIXME: we're hashing blocks elsewhere, factor out the hash fn */
+static unsigned hash_delta(block_t b)
 {
-	size_t len;
+	return (b * PRIME) & MASK;
+}
+
+static struct delta_entry *find_delta(struct space_map *sm, block_t b)
+{
+	struct list *l;
+	list_iterate (l, sm->buckets + hash_delta(b)) {
+		struct delta_entry *de = list_struct_base(l, struct delta_entry, hash);
+
+		if (de->block == b)
+			return de;
+	}
+
+	return NULL;
+}
+
+static int add_delta(struct space_map *sm, block_t b, int32_t delta)
+{
+	struct delta_entry *de = find_delta(sm, b);
+
+	if (!de) {
+		de = malloc(sizeof(*de));
+		if (!de)
+			return 0;
+
+		list_init(&de->lru);
+		list_add(sm->buckets + hash_delta(b), &de->hash);
+		de->block = b;
+		de->delta = 0;
+	}
+
+	list_move(&sm->deltas, &de->lru);
+	de->delta += delta;
+	return 1;
+}
+
+static int32_t get_delta(struct space_map *sm, block_t b)
+{
+	struct delta_entry *de = find_delta(sm, b);
+	return de ? de->delta : 0;
+}
+
+static struct space_map *sm_alloc(struct transaction_manager *tm, block_t nr_blocks)
+{
+	unsigned i;
 	struct space_map *sm = malloc(sizeof(*sm));
 
 	if (!sm)
 		return NULL;
 
+	sm->tm = tm;
 	sm->nr_blocks = nr_blocks;
-	sm->last_allocated = 1;	/* You can't allocate block 0 */
+	sm->initialised = 1;
+	sm->last_allocated = 0;
 
-	len = sizeof(*sm->ref_count) * nr_blocks;
-	sm->ref_count = malloc(len);
-	if (!sm->ref_count) {
-		free(sm);
-		return NULL;
-	}
-	memset(sm->ref_count, 0, len);
+	list_init(&sm->deltas);
+	for (i = 0; i < NR_BUCKETS; i++)
+		list_init(sm->buckets + i);
 
 	return sm;
 }
 
-void space_map_destroy(struct space_map *sm)
+struct space_map *sm_new(struct transaction_manager *tm,
+			 block_t nr_blocks)
 {
-	free(sm->ref_count);
+	struct space_map *sm = sm_alloc(tm, nr_blocks);
+	if (sm) {
+		sm->initialised = 0;
+		// sm_inc_block(sm, 0);	/* FIXME: reserve */
+	}
+
+	return sm;
+}
+
+struct space_map *sm_open(struct transaction_manager *tm,
+			  block_t root, block_t nr_blocks)
+{
+	struct space_map *sm = sm_alloc(tm, nr_blocks);
+	if (sm)
+		sm->root = root;
+	return sm;
+}
+
+void sm_close(struct space_map *sm)
+{
+	struct list *l, *tmp;
+	list_iterate_safe (l, tmp, &sm->deltas) {
+		struct delta_entry *de = list_struct_base(l, struct delta_entry, hash);
+		free(de);
+	}
+
 	free(sm);
+}
+
+static void next_block(struct space_map *sm)
+{
+	sm->last_allocated++;
+	if (sm->last_allocated == sm->nr_blocks)
+		sm->last_allocated = 0;
 }
 
 int sm_new_block(struct space_map *sm, block_t *b)
 {
-	uint32_t i;
+	block_t i;
 
-	/* FIXME: duplicate code in these loops */
-	for (i = sm->last_allocated; i < sm->nr_blocks; i++)
-		if (sm->ref_count[i] == 0) {
-			sm->ref_count[i] = 1;
-			*b = sm->last_allocated = i;
+	for (i = 0; i < sm->nr_blocks; i++) {
+		uint64_t ref_count = 0;
+		int32_t delta;
+
+		next_block(sm);
+		delta = get_delta(sm, sm->last_allocated);
+		if (delta > 0)
+			continue;
+
+		if (sm->initialised)
+			if (btree_lookup_equal(sm->tm, sm->root,
+					       sm->last_allocated, &ref_count) == LOOKUP_ERROR)
+				return 0;
+
+		if (ref_count + delta == 0) {
+			if (!sm_inc_block(sm, sm->last_allocated))
+				return 0;
+			*b = sm->last_allocated;
 			return 1;
 		}
-
-	for (i = 1; i < sm->last_allocated; i++)
-		if (sm->ref_count[i] == 0) {
-			sm->ref_count[i] = 1;
-			*b = sm->last_allocated = i;
-			return 1;
-		}
+	}
 
 	return 0;
 }
 
 int sm_inc_block(struct space_map *sm, block_t b)
 {
-	sm->ref_count[b]++;
-	return 1;
+	assert(b);
+	return add_delta(sm, b, 1);
 }
 
 int sm_dec_block(struct space_map *sm, block_t b)
 {
-	assert(sm->ref_count[b]);
-	sm->ref_count[b]--;
-	return 1;
+	return add_delta(sm, b, -1);
 }
 
 uint32_t sm_get_count(struct space_map *sm, block_t b)
 {
-	return sm->ref_count[b];
+	uint64_t rc = 0;
+
+	if (sm->initialised && !btree_lookup_equal(sm->tm, sm->root, b, &rc))
+		abort();
+
+	return rc + get_delta(sm, b);
 }
 
+static int flush_once(struct space_map *sm)
+{
+	struct list head, *l;
+	block_t root = sm->root;
+
+	list_init(&head);
+	list_splice(&head, &sm->deltas);
+
+	list_iterate (l, &head) {
+		struct delta_entry *de = list_struct_base(l, struct delta_entry, hash);
+		uint32_t shadow = de->delta;
+		uint64_t value;
+
+		switch (btree_lookup_equal(sm->tm, root, de->block, &value)) {
+		case LOOKUP_ERROR:
+			return 0;
+
+		case LOOKUP_NOT_FOUND:
+			if (!btree_insert(sm->tm, root, de->block, shadow, &root))
+				return 0;
+			break;
+
+		case LOOKUP_FOUND:
+			if (!btree_insert(sm->tm, root, de->block, shadow + value, &root))
+				return 0;
+			break;
+		}
+
+		/*
+		 * The delta may have increased as a result of the btree
+		 * operations above.  So we subtract shadow, rather than
+		 * setting to 0.
+		 */
+		de->delta -= shadow;
+	}
+
+	sm->root = root;
+	return 1;
+}
+
+int sm_flush(struct space_map *sm, block_t *new_root)
+{
+	unsigned i;
+	if (!sm->initialised) {
+		if (!btree_empty(sm->tm, &sm->root))
+			return 0;
+		sm->initialised = 1;
+	}
+
+	while (!list_empty(&sm->deltas))
+		if (!flush_once(sm))
+			return 0;
+
+	for (i = 0; i < NR_BUCKETS; i++) {
+		struct list *l, *tmp;
+		list_iterate_safe (l, tmp, sm->buckets + i) {
+			struct delta_entry *de = list_struct_base(l, struct delta_entry, hash);
+			free(de);
+		}
+		list_init(sm->buckets + i);
+	}
+
+	*new_root = sm->root;
+	return 1;
+}
+
+#if 0
 void sm_dump(struct space_map *sm)
 {
 	size_t len = sm->nr_blocks * sizeof(uint32_t);
@@ -132,5 +308,5 @@ void sm_dump_comparison(struct space_map *sm1, struct space_map *sm2)
 			printf("counts for block %u differ %u/%u\n",
 			       i, (unsigned) sm1->ref_count[i], (unsigned) sm2->ref_count[i]);
 }
-
+#endif
 /*----------------------------------------------------------------*/
--- src/snapshots/src/space_map.h
+++ src/snapshots/src/space_map.h
@@ -2,27 +2,65 @@
 #define SNAPSHOTS_SPACE_MAP_H
 
 #include "snapshots/types.h"
+#include "snapshots/transaction_manager.h"
 
 /*----------------------------------------------------------------*/
 
 /*
- * This in-core data structure keeps a record of how many times each block
- * in a device is referenced.
+ * This structure keeps a record of how many times each block in a device
+ * is referenced.  It needs to be persisted to disk as part of the
+ * transaction.
+ *
+ * Writing the space map is a challenge.  It is used extensively by the
+ * transaction manager, but we'd also like to implement its on-disk format
+ * using the standard data structures such as the btree.  So we can easily
+ * get into cycles.  For example:
+ *
+ * snapshot -> btree -> tm_shadow -> space_map_alloc -> btree -> shadow -> space_map alloc
+ *
+ * How do we break this cycle?  Have 2 modes for the space map to operate
+ * in: IN_CORE and then a FLUSH_TO_DISK.  This just defers the cycle,
+ * instead of being triggered by the snapshot, it'll be hit when the flush
+ * is done:
+ *
+ * sm_flush -> btree -> tm_shadow -> space_map_alloc
+ *
+ * We get round this by (internally) running flush to write the allocations
+ * caused by the snap client.  Then again to write the allocations caused
+ * by the first flush etc.  Then again to write allocations from second
+ * flush etc.  A good on disk format will be one that minimises the number
+ * of cycles (possibly not a btree).
  */
+
 struct space_map;
 
-struct space_map *space_map_create(block_t nr_blocks);
-void space_map_destroy(struct space_map *sm);
+/* Create a new space smap */
+struct space_map *sm_new(struct transaction_manager *tm,
+			 block_t nr_blocks);
 
+/* Open a pre-existing space map at the given root */
+/* FIXME: remove the nr_blocks arg */
+struct space_map *sm_open(struct transaction_manager *tm,
+			  block_t root, block_t nr_blocks);
+void sm_close(struct space_map *sm);
+
+int sm_get_root(struct space_map *sm, block_t *root);
+
+/*
+ * These can access the disk structures, but only to read the existing
+ * layout.  In in core journal notes what reference count deltas have been
+ * made.  You must call flush if you want it to hit the disk.
+ */
 int sm_new_block(struct space_map *sm, block_t *b);
 int sm_inc_block(struct space_map *sm, block_t b);
 int sm_dec_block(struct space_map *sm, block_t b);
 uint32_t sm_get_count(struct space_map *sm, block_t b);
 
+int sm_flush(struct space_map *sm, block_t *new_root);
+
 /*----------------------------------------------------------------*/
 
 /* Debug only */
-
 void sm_dump(struct space_map *sm);
 int sm_equal(struct space_map *sm1, struct space_map *sm2);
 void sm_dump_comparison(struct space_map *sm1, struct space_map *sm2);
