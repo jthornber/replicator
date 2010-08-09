507ddd93b802275d4a6109313d9a25748af1bb7c
Joe Thornber <joe.thornber@gmail.com>
2010-08-09 16:16:45 +0100

    [SNAPSHOT] trivial implementation of a device register

--- src/snapshots/src/device.c
+++ src/snapshots/src/device.c
@@ -1,11 +1,58 @@
 #include "device.h"
 
+#include "datastruct/list.h"
+
 /*----------------------------------------------------------------*/
 
-int dev_register(dev_t d, const char *path);
-int dev_open(dev_t d);
-int dev_close(dev_t d);
-int dev_size(int fd);
+struct dlist {
+	struct list list;
+	dev_t dev;
+	int fd;
+};
 
+struct device_register {
+	struct list devs;
+};
+
+struct device_register *device_register_create()
+{
+	struct device_register *dr = malloc(sizeof(*dr));
+	if (dr)
+		list_init(&dr->devs);
+	return dr;
+}
+
+void device_register_destroy(struct device_register *dr)
+{
+	struct dlist *dl, *tmp;
+
+	list_iterate_items_safe (dl, tmp, &dr->devs)
+		free(dl);
+	free(dr);
+}
+
+int dev_register(struct device_register *dr, dev_t d, int fd)
+{
+	struct dlist *dl = malloc(sizeof(*dl));
+	if (!dl)
+		return 0;
+
+	dl->dev = d;
+	dl->fd = fd;
+	list_add(&dr->devs, &dl->list);
+
+	return 1;
+}
+
+/* -1 on error */
+int dev_lookup(struct device_register *dr, dev_t d)
+{
+	struct dlist *dl;
+
+	list_iterate_items (dl, &dr->devs)
+		if (dl->dev == d)
+			return dl->fd;
+	return -1;
+}
 
 /*----------------------------------------------------------------*/
--- src/snapshots/src/device.h
+++ src/snapshots/src/device.h
@@ -6,13 +6,16 @@
 /*----------------------------------------------------------------*/
 
 typedef uint64_t dev_t;
+struct device_register;
+
+struct device_register *device_register_create();
+void device_register_destroy(struct device_register *);
 
 /* FIXME: hack, revisit */
-int dev_register(dev_t d, const char *path);
+int dev_register(struct device_register *dr, dev_t d, int fd);
 
-int dev_size(dev_t d);
-int dev_open(dev_t d);
-int dev_close(dev_t d);
+/* -1 on error */
+int dev_lookup(struct device_register *dr, dev_t d);
 
 /*----------------------------------------------------------------*/
 
