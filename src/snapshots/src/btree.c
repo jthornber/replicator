0d9b54c3e17dcd8b9f42666505dd0230a09983fc
Joe Thornber <joe.thornber@gmail.com>
2010-10-08 11:22:38 +0100

    [SNAPSHOTS] more work on btree_merge

--- src/snapshots/src/btree.c
+++ src/snapshots/src/btree.c
@@ -25,6 +25,11 @@ static void array_insert(void *base, size_t elt_size, unsigned nr_elts,
 
 /*----------------------------------------------------------------*/
 
+static uint64_t *key_ptr(struct node *n, uint32_t index)
+{
+	return n->keys + index;
+}
+
 static void *value_base(struct node *n)
 {
 	return &n->keys[n->header.max_entries];
@@ -98,6 +103,23 @@ static void insert_at(size_t value_size,
 	array_insert(node->keys, sizeof(*node->keys), node->header.nr_entries, index, &key);
 	array_insert(value_base(node), value_size, node->header.nr_entries, index, value);
 	node->header.nr_entries++;
+}
+
+static void delete_at(struct node *n, unsigned index, size_t value_size)
+{
+	unsigned nr_to_copy = n->header.nr_entries - index - 1;
+
+	if (nr_to_copy) {
+		memmove(key_ptr(n, index),
+			key_ptr(n, index + 1),
+			nr_to_copy);
+
+		memmove(value_ptr(n, index, value_size),
+			value_ptr(n, index + 1, value_size),
+			nr_to_copy);
+	}
+
+	n->header.nr_entries--;
 }
 
 /*----------------------------------------------------------------*/
@@ -725,7 +747,8 @@ int btree_insert(struct btree_info *info, block_t root,
 	return 1;
 }
 
-#if 0
+/*----------------------------------------------------------------*/
+
 /* FIXME: move this all to a separate file ? */
 struct maybe_count {
 	int has_value;
@@ -748,9 +771,10 @@ static int is_just(struct maybe_count *c)
 	return c->has_value;
 }
 
-static int get_entry_count(struct node *parent, int index, struct maybe_count *c)
+static int get_entry_count(struct btree_info *info,
+			   struct node *parent, int index, struct maybe_count *c)
 {
-	struct node_block nb;
+	struct block_node nb;
 
 	if ((index < 0) ||
 	    (index > parent->header.nr_entries - 1)) {
@@ -758,20 +782,61 @@ static int get_entry_count(struct node *parent, int index, struct maybe_count *c
 		return 1;
 	}
 
-	nb.b = value64(p->n, parent_index - 1);
-	if (!read_lock(&nb))
+	nb.b = value64(parent, index - 1);
+	if (!read_lock(info, &nb))
 		return 0;
 
 	just(c, nb.n->header.nr_entries);
-	if (!unlock(&nb))
+	if (!unlock(info, &nb))
 		return 0;
 
 	return 1;
 }
 
-static int btree_merge(struct shadow_spine *s, unsigned parent_index, size_t value_size)
+/*
+ * Moves |count| entries from |l| into |c|
+ */
+static void merge_from_left(struct node *c, struct node *l, unsigned count, size_t value_size)
 {
-	struct node *p = shadow_parent(s), *c = shadow_current(s);
+	unsigned left_index = l->header.nr_entries - count;
+
+	memmove(key_ptr(c, count),
+		key_ptr(c, 0),
+		c->header.nr_entries * sizeof(uint64_t));
+	memcpy(key_ptr(c, 0),
+	       key_ptr(l, left_index),
+	       count * sizeof(uint64_t));
+
+	memmove(value_ptr(c, count, value_size),
+		value_ptr(c, 0, value_size),
+		c->header.nr_entries * value_size);
+	memcpy(value_ptr(c, 0, value_size),
+	       value_ptr(l, left_index, value_size),
+	       count * value_size);
+}
+
+/*
+ * Moves |count| entries from |r| into |c|
+ */
+static void merge_from_right(struct node *c, struct node *r, unsigned count, size_t value_size)
+{
+	memcpy(key_ptr(c, c->header.nr_entries),
+	       key_ptr(r, 0),
+	       count * sizeof(uint64_t));
+
+	memcpy(value_ptr(c, c->header.nr_entries, value_size),
+	       value_ptr(r, 0, value_size),
+	       count * value_size);
+}
+
+static unsigned merge_threshold(struct node *n)
+{
+	return n->header.max_entries / 2;
+}
+
+int btree_node_merge(struct shadow_spine *s, unsigned parent_index, size_t value_size)
+{
+	struct node *p = shadow_parent(s)->n, *c = shadow_current(s)->n;
 	struct maybe_count left_count, right_count;
 
 	/* you can't merge the root node */
@@ -779,8 +844,8 @@ static int btree_merge(struct shadow_spine *s, unsigned parent_index, size_t val
 		return 0;
 
 	/* look at the neighbouring nodes */
-	if (!get_entry_count(p->n, ((int) parent_index) - 1, &left_count) ||
-	    !get_entry_count(p->n, ((int) parent_index) + 1, &right_count))
+	if (!get_entry_count(s->info, p, ((int) parent_index) - 1, &left_count) ||
+	    !get_entry_count(s->info, p, ((int) parent_index) + 1, &right_count))
 		return 0;
 
 	/* merge scenarios:
@@ -790,48 +855,107 @@ static int btree_merge(struct shadow_spine *s, unsigned parent_index, size_t val
 	 * 4) move some of left into current
 	 * 5) move some of right into current
 	 */
-	if (is_just(&left_count)) {
-		if (left_count.value < merge_threshold(c->n)) {
-			/* scenario 1 */
-			struct block_node left;
-			left.b = value64(p->n, parent_index - 1);
-			if (!read_lock(&left)) {
-				return 0;
-			}
-
-			memmove(c->n->keys + (sizeof(uint64_t) * left_count.value),
-				c->n->keys,
-				c->n->header.nr_entries * sizeof(uint64_t));
-			memcpy(c->n->keys,
-			       left.n->keys,
-			       left_count.value * sizeof(uint64_t));
-
-			memmove(value_ptr(c->n, left_count.value, value_size),
-				value_ptr(c->n, 0, value_size),
-				c->n->header.nr_entries * );
+	if (is_just(&left_count) && (left_count.value < merge_threshold(c))) {
+		/* scenario 1 */
+		struct block_node left;
+		left.b = value64(p, parent_index - 1);
+		if (!read_lock(s->info, &left)) {
+			return 0;
 		}
-	}
 
-	if (is_just(&right_count)) {
-		if (right_count.value < merge_threshold(c->n)) {
-			/* scenario 2 */
+		merge_from_left(c, left.n, left_count.value, value_size);
+		*key_ptr(p, parent_index) = *key_ptr(c, 0);
+		unlock(s->info, &left);
+
+		// patch up the parent
+		tm_dec(s->info->tm, left.b);
+		delete_at(p, parent_index - 1, value_size);
+
+	} else if (is_just(&right_count) && (right_count.value < merge_threshold(c))) {
+		/* scenario 2 */
+		struct block_node right;
+		right.b = value64(p, parent_index + 1);
+		if (!read_lock(s->info, &right)) {
+			return 0;
 		}
-	}
 
-	if (is_just(&left_count) && is_just(&right_count)) {
+		merge_from_right(c, right.n, right_count.value, value_size);
+		unlock(s->info, &right);
+
+		// patch up the parent
+		tm_dec(s->info->tm, right.b);
+		delete_at(p, parent_index + 1, value_size);
+
+	} else if (is_just(&left_count) && is_just(&right_count)) {
 		/* scenario 3 */
-	}
+		unsigned total_entries = left_count.value +
+			right_count.value +
+			c->header.nr_entries;
+		unsigned per_node = total_entries / 3;
 
-	if (is_just(&left_count)) {
+		assert(per_node >= merge_threshold(c));
+
+		struct block_node left, right;
+		left.b = value64(p, parent_index - 1);
+		if (!read_lock(s->info, &left)) {
+			return 0;
+		}
+
+		right.b = value64(p, parent_index + 1);
+		if (!read_lock(s->info, &right)) {
+			unlock(s->info, &left);
+			return 0;
+		}
+
+		merge_from_left(c, left.n, left_count.value - per_node, value_size);
+		merge_from_right(c, right.n, total_entries - (2 * per_node), value_size);
+		*key_ptr(p, parent_index) = *key_ptr(c, 0);
+		*key_ptr(p, parent_index + 1) = *key_ptr(right.n, 0);
+
+		unlock(s->info, &left);
+		unlock(s->info, &right);
+
+	} else if (is_just(&left_count)) {
 		/* scenario 4 */
+		unsigned per_node = (left_count.value + c->header.nr_entries) / 2;
+		struct block_node left;
+		left.b = value64(p, parent_index - 1);
+		if (!read_lock(s->info, &left)) {
+			return 0;
+		}
+
+		merge_from_left(c, left.n, left_count.value - per_node, value_size);
+		*key_ptr(p, parent_index) = *key_ptr(c, 0);
+
+		unlock(s->info, &left);
 
 	} else {
 		/* scenario 5 */
+		unsigned per_node = (right_count.value + c->header.nr_entries) / 2;
+
+		struct block_node right;
+		right.b = value64(p, parent_index + 1);
+		if (!read_lock(s->info, &right)) {
+			return 0;
+		}
+
+		merge_from_right(c, right.n, right_count.value - per_node, value_size);
+		*key_ptr(p, parent_index + 1) = *key_ptr(right.n, 0);
+
+		unlock(s->info, &right);
 	}
+
+	/*
+	 * if p now has a single entry, and c still has <= merge_threshold
+	 * entries.  Then we copy c to p.
+	 */
+	/* FIXME: finish */
 
 	return 1;
 }
 
+#if 0
+/* FIXME: single layer for now */
 int btree_remove(struct btree_info *info,
 		 block_t root, uint64_t *keys,
 		 block_t *new_root)
@@ -853,18 +977,21 @@ int btree_remove(struct btree_info *info,
 			inc_children(info, node, fn);
 
 		if (node->header.nr_entries < node->header.max_entries / 2) {
-			/* fixup code here */
-
-			/* patch up parent */
+			size_t value_size = node->header.flags & NODE_INTERNAL ? sizeof(uint64_t) : info->value_size;
+			if (!btree_merge(&spine, parent_index, value_size))
+				abort();
 
 			/* possibly patch up root */
+			// FIXME: think about this
 		}
 
 		dup_block = *block;
-
-
+	}
 }
 #endif
+
+/*----------------------------------------------------------------*/
+
 int btree_clone(struct btree_info *info, block_t root, block_t *clone)
 {
 	struct node *clone_node, *orig_node;
